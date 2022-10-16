2b20bc7d4fb2a8b0fc5997246c9986b4b52fbe6b
Yim Sotharoth <yimsotharoth999@gmail.com>
2022-10-16 19:44:00 +0700

    useEncrypt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import useLocalStorage from './useLocalStorage';
 import useUpdateLogger from './useUpdateLogger';
+import useEncrypt from './useEncrypt';
 
 function App() {
 
   const [name,setName] = useLocalStorage('name','');
   useUpdateLogger(name);
+  console.log(useEncrypt("HELLO WORLD"));
 
   return (
     <input
